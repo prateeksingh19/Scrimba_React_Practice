c3ace697b8bbd7e6e57f9a558ef04f8701fd6fe4
Prateek Singh <prateekvns99@gmail.com>
2024-02-12 14:41:34 +0530

    Added some functionalities

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -7,7 +7,34 @@ import "./App.css";
 export default function App() {
   const [dice, setDice] = useState(createNewDie());
 
+  const [rolls, setRolls] = useState(0);
+
+  const [time, setTime] = useState(0);
+
   const [tenzies, setTenzies] = useState(false);
+
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  useEffect(() => {
+    let timerId;
+    if (timerStarted && !tenzies) {
+      timerId = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(timerId);
+  }, [timerStarted, tenzies]);
+
+  useEffect(() => {
+    if (tenzies) {
+      const bestTime = JSON.parse(localStorage.getItem("time"));
+      const parsedBestTime = !isNaN(bestTime) ? bestTime : 0;
+      if (parsedBestTime > time) {
+        localStorage.setItem("time", JSON.stringify(time));
+      }
+    }
+  }, [tenzies]);
 
   useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
@@ -29,7 +56,7 @@ export default function App() {
   function createNewDie() {
     const newDie = [];
     for (let i = 0; i < 10; i++) {
-      newDie.push(generateNewDie());
+      newDie.push({ id: nanoid(), value: 0, isHeld: false });
     }
     return newDie;
   }
@@ -41,20 +68,30 @@ export default function App() {
           return die.isHeld ? die : generateNewDie();
         })
       );
+      setRolls((prevRoll) => prevRoll + 1);
+      if (!timerStarted) {
+        setTimerStarted(true);
+      }
     } else {
       setTenzies(false);
       setDice(createNewDie);
+      setRolls(0);
+      setTime(0);
+      setTimerStarted(false);
     }
   }
 
   function holdDice(id) {
-    setDice((oldDie) =>
-      oldDie.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-      })
-    );
+    if (timerStarted) {
+      setDice((oldDie) =>
+        oldDie.map((die) => {
+          return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+        })
+      );
+    } else {
+      alert("Please roll the dice first to start the game!");
+    }
   }
-
   const diceElements = dice.map((die) => (
     <Die
       key={die.id}
@@ -77,6 +114,11 @@ export default function App() {
         <button onClick={rollDice} className="rollDice">
           {tenzies ? "New Game" : "Roll"}
         </button>
+        <div className="stats">
+          <p>{`No. of Rolls: ${rolls}`}</p>
+          <p>{`Time taken: ${time}`}</p>
+          <p>{`Best Time: ${JSON.parse(localStorage.getItem("time")) || 0}`}</p>
+        </div>
       </div>
     </main>
   );
